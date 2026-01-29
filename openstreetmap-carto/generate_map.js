a5e29c50c0e27da6aa3e898906a42a05e08a49c7
dmmettlach <drew@mettlach.codes>
2026-01-28 21:41:11 -0700

    Make test map bigger

--- openstreetmap-carto/generate_map.js
+++ openstreetmap-carto/generate_map.js
@@ -5,19 +5,21 @@ const mapnik = require('@mapnik/mapnik');
 mapnik.register_default_fonts();
 mapnik.register_default_input_plugins();
 
-var map = new mapnik.Map(256, 256);
-map.load('./mapnik.xml', function(err,map) {
+var map = new mapnik.Map(512, 512);
+map.load('./mapnik.xml', function(err, map) {
+  if (err) throw err;
+  map.zoomAll();
+
+
+  var im = new mapnik.Image(256, 256);
+  map.render(im, function(err, im) {
     if (err) throw err;
-    map.zoomAll();
-    var im = new mapnik.Image(256, 256);
-    map.render(im, function(err,im) {
+    im.encode('png', function(err, buffer) {
       if (err) throw err;
-      im.encode('png', function(err,buffer) {
-          if (err) throw err;
-          fs.writeFile('map.png',buffer, function(err) {
-              if (err) throw err;
-              console.log('saved map image to map.png');
-          });
+      fs.writeFile('map.png', buffer, function(err) {
+        if (err) throw err;
+        console.log('saved map image to map.png');
       });
     });
+  });
 });
