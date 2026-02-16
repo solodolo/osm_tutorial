1b2666ac679e3af09f3e47f235aad7a26390fdc8
dmmettlach <drew@mettlach.codes>
2026-02-15 21:30:02 -0700

    Change style of selected wiki node

--- wiki-map/main.ts
+++ wiki-map/main.ts
@@ -7,6 +7,12 @@ import View from 'ol/View.js';
 import TileLayer from 'ol/layer/Tile.js';
 import VectorTileLayer from 'ol/layer/VectorTile.js';
 import VectorTileSource from 'ol/source/VectorTile.js';
+import Select from 'ol/interaction/Select.js';
+import { Fill, Stroke, Style } from 'ol/style';
+import CircleStyle from 'ol/style/Circle';
+import { StyleLike } from 'ol/style/Style';
+import { click } from 'ol/events/condition';
+import { FeatureLike } from 'ol/Feature';
 
 const map: OLMap = new OLMap({
   view: new View({
@@ -20,17 +26,37 @@ const layer: TileLayer = new TileLayer({
   source: new OSM(),
 });
 
+const defaultStyle = new Style({
+  image: new CircleStyle({
+    radius: 3,
+    fill: new Fill({ color: '#3c6dce' }),
+    stroke: new Stroke({ color: 'white', width: 0.5 }),
+  }),
+});
+
+const selectedStyle = new Style({
+  image: new CircleStyle({
+    radius: 8,
+    fill: new Fill({ color: '#3c6dce' }),
+    stroke: new Stroke({ color: 'black', width: 1.5 }),
+  }),
+});
+
+let selectedFeature = null;
 const layer2 = new VectorTileLayer({
   source: new VectorTileSource({
     format: new MVT(),
     url: "http://127.0.0.1:7800/public.enwiki_page_geo_by_zoom_advanced/{z}/{x}/{y}.pbf",
     maxZoom: 22,
   }),
-  style: {
-    'circle-radius': 3,
-    'circle-fill-color': '#3c6dce',
-    'circle-stroke-color': 'white',
-    'circle-stroke-width': 0.5,
+  style: (feature) => {
+    let pageId = feature?.get('page_id');
+
+    if (selectedFeature == pageId) {
+      return selectedStyle;
+    }
+
+    return defaultStyle;
   },
 });
 
@@ -119,7 +145,17 @@ async function fetchWikiPreview(pageId: number): Promise<WikiPreview | null> {
   return preview;
 }
 
-map.on('click', function(event) {
+function resetSelectedFeatures() {
+  selectedFeature = null;
+  layer2.changed();
+}
+
+function setSelectedFeature(feature: FeatureLike) {
+  selectedFeature = feature.get('page_id');
+  layer2.changed();
+}
+
+function mapClickListener(event) {
   const hit = map.forEachFeatureAtPixel(
     event.pixel,
     (feature) => feature,
@@ -132,9 +168,12 @@ map.on('click', function(event) {
   );
 
   if (!hit) {
+    resetSelectedFeatures();
     closeTooltip();
     return;
   }
+
+  setSelectedFeature(hit);
 
   const pageIdRaw = (hit as any).get?.('page_id');
   const pageId = Number(pageIdRaw);
@@ -169,4 +208,7 @@ map.on('click', function(event) {
         `<div class="wiki-tooltip__title">Failed to load preview</div>`,
       );
     });
-});
+}
+
+map.on('click', mapClickListener);
+
