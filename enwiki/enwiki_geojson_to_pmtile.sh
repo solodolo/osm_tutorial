b8b1caa64e55830a5e255b205e44e2980daf8179
dmmettlach <drew@mettlach.codes>
2026-08-26 19:51:25 -0700

    Fix cluster distance so that multiple tooltips don't open at once

--- enwiki/enwiki_geojson_to_pmtile.sh
+++ enwiki/enwiki_geojson_to_pmtile.sh
@@ -1,3 +1,3 @@
 #! /usr/bin/env bash
 
-tippecanoe -zg -o enwiki_page_geo.pmtiles --force --drop-densest-as-needed --extend-zooms-if-still-dropping enwiki_page_geo.jsonl
+tippecanoe -zg -o enwiki_page_geo.pmtiles --force --cluster-distance=0 --drop-densest-as-needed --extend-zooms-if-still-dropping enwiki_page_geo.jsonl
